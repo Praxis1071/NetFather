c44cb53f2d090a03b3ad89c8085ee844918cbe85
Mehmet Boztepe <mehmet.boztepe.tech@gmail.com>
2026-09-25 11:46:35 +0300

    test: fix TCP server readiness ordering

--- tests/integration/test_firewall_netns.py
+++ tests/integration/test_firewall_netns.py
@@ -23,8 +23,8 @@ def test_nftables_policy_applies_to_an_existing_tcp_connection():
     server_script = (
         "import socket,time\n"
         f"s=socket.socket(); s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1); s.bind(('10.203.0.2',{port})); s.listen(1); "
-        "s.settimeout(8); c,_=s.accept(); c.settimeout(2); "
-        f"open('{ready}','w').close(); "
+        "s.settimeout(8); "
+        f"open('{ready}','w').close(); c,_=s.accept(); c.settimeout(2); "
         "data=[]\n"
         "while True:\n"
         "  try:\n"
