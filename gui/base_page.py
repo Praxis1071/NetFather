f88562bdb8ff7c5858b4cba0b834a58745ca8846
Mehmet Boztepe <mehmet.boztepe.tech@gmail.com>
2026-09-25 11:26:29 +0300

    refactor: extract shared GTK page primitives

---
+++ gui/base_page.py
@@ -0,0 +1,29 @@
+"""Shared GTK4 page primitives."""
+from __future__ import annotations
+
+from gi.repository import Gtk
+
+
+class BasePage(Gtk.Box):
+    def __init__(self, title: str, subtitle: str = "") -> None:
+        super().__init__(orientation=Gtk.Orientation.VERTICAL, spacing=18)
+        self.set_margin_top(28)
+        self.set_margin_bottom(28)
+        self.set_margin_start(32)
+        self.set_margin_end(32)
+        heading = Gtk.Label(label=title, xalign=0)
+        heading.add_css_class("title-1")
+        self.append(heading)
+        if subtitle:
+            label = Gtk.Label(label=subtitle, xalign=0, wrap=True)
+            label.add_css_class("dim-label")
+            self.append(label)
+
+
+def section(title: str, child: Gtk.Widget) -> Gtk.Box:
+    box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
+    heading = Gtk.Label(label=title, xalign=0)
+    heading.add_css_class("heading")
+    box.append(heading)
+    box.append(child)
+    return box
